fe34a44abda8022593bad002729e176eb3245b54
Rohit IITB <rohitg@cse.iitb.ac.in>
2014-07-14 20:42:46 +0530

    Communication till sm processor node done

--- cpp/main.h
+++ cpp/main.h
@@ -11,7 +11,7 @@
 #define POWER_POOL Pool<powerEvent,CompareTs>
 
 #define print_debug(level,fmt, ...) \
-            do { if (level <= DEBUG_LEVEL) {fprintf(stderr, "[Debug %d]%s | line %d | ",level,__FILE__,__LINE__); fprintf(stderr, fmt, __VA_ARGS__); }} while (0)
+            do { if (level <= DEBUG_LEVEL) {fprintf(stdout, "[Debug %d]%s | line %d | ",level,__FILE__,__LINE__); fprintf(stdout, fmt, __VA_ARGS__); }} while (0)
 
 #define print_error(fmt, ...) \
             do { fprintf(stderr, "[Error]|%s|%d|",__FILE__,__LINE__); fprintf(stderr, fmt, __VA_ARGS__); } while (0)
