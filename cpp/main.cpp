f2cde3829adf9605127c155e6e2d8a15f527ff6e
Rohit IITB <rohitg@cse.iitb.ac.in>
2014-07-14 20:42:37 +0530

    Communication till sm processor node done

--- cpp/main.cpp
+++ cpp/main.cpp
@@ -2,6 +2,7 @@
 #include "priorityPool.h"
 #include "nodeProcessor.h"
 #include "messageRouter.h"
+#include "Reader1.h"
 
 int main(int argc, char **argv)
 {
@@ -11,12 +12,13 @@ int main(int argc, char **argv)
 }
 
 void createNetwork(){
-//	1	This function will load network from database
-//	2	create the pipes
-//	3	create threads
-//	4	Create reader thread  
-//	5	update thread with pipes
-//	6	start threads and wait for them to finish
+//	change	status	desc
+//	1	done	This function will load network from database
+//	2	done	create the pipes
+//	3	done	create threads
+//	4	not	Create reader thread  
+//	5	not	update thread with pipes
+//	6	not	start threads and wait for them to finish
   
 //	[1]
 	std::vector<node> net;
@@ -28,9 +30,15 @@ void createNetwork(){
 //	itrate over each node in the "net" and create a pool for parent 
 //	put these links into the links map
 //	create processor thread for each parent using these links as "inLink"
-	messageRouter r1;
+	
+	POWER_POOL *routerLink;
+	routerLink = new  POWER_POOL(5,50,9999);
+	
+	messageRouter r1(9999,routerLink);
+	Reader1 fr1(9998,routerLink);
+	
 	POWER_POOL *link1;
-	std::map<int,POWER_POOL > links;
+	std::map<int,POWER_POOL *> links;
 	nodeProcessor * processor;
 	std::map<int,nodeProcessor> processors;
 	
@@ -38,8 +46,8 @@ void createNetwork(){
 	for (std::vector<node>::iterator it = net.begin() ; it != net.end(); ++it){
 		if (links.count(it->parentID) == 0){
 			print_debug(2,"Making link for %d\n",it->parentID);
-			link1 = new POWER_POOL(50,50,it->parentID);
-			links[it->parentID] = *link1;
+			link1 = new POWER_POOL(5,50,it->parentID);
+			links[it->parentID] = link1;
 			processor = new nodeProcessor(it->parentID,link1);
 			processors[it->parentID] = *processor;
 			higherNodes[it->parentID] = 1;
@@ -58,21 +66,42 @@ void createNetwork(){
 		if (links.count(it->nodeID) != 0){
 			print_debug(2,"link exist %d \n",it->nodeID);
 
-			processors[it->nodeID].addOutLink(it->parentID,&links[it->parentID]);
+			processors[it->nodeID].addOutLink(it->parentID,links[it->parentID]);
 			// also add reverse path
-			processors[it->parentID].addChildLink(it->nodeID,&links[it->nodeID]);
+			processors[it->parentID].addChildLink(it->nodeID,links[it->nodeID]);
 			
 		}
 		// If node is not a parent add entry and parent's link to router 
 		else{
 			print_debug(2,"link absent %d \n",it->nodeID);
 			// parent link might already be added
-			r1.addTargetLink(it->parentID,&links[it->parentID]);
+			r1.addTargetLink(it->parentID,links[it->parentID]);
 			r1.addTarget(it->nodeID,it->parentID);
 		}
 		
 		
 	}	
+	// call init for all threads
+	print_debug(1,"-----------------Initialization complete------------------ %d \n",0);
+	fr1.init();
+	r1.init();
+
+	//std::map<int,nodeProcessor> processors;
+	//std::map<int,nodeProcessor>::iterator it;
+	for (std::map<int,nodeProcessor>::iterator it = processors.begin() ; it != processors.end(); ++it){
+		it->second.init();
+	}
+	
+
+	// call join for all threads
+	fr1.join();
+	r1.join();
+
+	//std::map<int,nodeProcessor> processors;
+	//std::map<int,nodeProcessor>::iterator it;
+	for (std::map<int,nodeProcessor>::iterator it = processors.begin() ; it != processors.end(); ++it){
+		it->second.join();
+	}
 	
 }
 
