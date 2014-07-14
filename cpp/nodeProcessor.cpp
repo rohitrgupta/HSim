c0d4528a63d7d75adde0bd711b200c73f0fa6c39
Rohit IITB <rohitg@cse.iitb.ac.in>
2014-07-14 20:45:10 +0530

    Communication till sm processor node done

--- cpp/nodeProcessor.cpp
+++ cpp/nodeProcessor.cpp
@@ -30,8 +30,8 @@ nodeProcessor::nodeProcessor(){
 }
 
 int nodeProcessor::addOutLink(int parent,POWER_POOL * outPool){
-	print_debug(2,"Adding parent link %d at node %d\n",parent,nodeID);
-	parentID = parent;
+	print_debug(2,"Adding parent link %d at node %d\n",parent,nodeId);
+	parentId = parent;
 	if (outLink != NULL){
 		outLink = outPool;
 		return 0;
@@ -43,10 +43,10 @@ int nodeProcessor::addOutLink(int parent,POWER_POOL * outPool){
 }
 
 int nodeProcessor::addChildLink(int child, POWER_POOL * childPool){
-	print_debug(2,"Adding child *link* %d at node %d \n",child,nodeID);
+	print_debug(2,"Adding child *link* %d at node %d \n",child,nodeId);
 
 	if (childLinks.count(child) == 0){
-		childLinks[child] = * childPool;
+		childLinks[child] = childPool;
 		return 0;
 	}
 	else{
@@ -57,7 +57,7 @@ int nodeProcessor::addChildLink(int child, POWER_POOL * childPool){
 }
 
 int nodeProcessor::addChild(int child){
-	print_debug(2,"Adding child %d at node %d \n",child,nodeID);
+	print_debug(2,"Adding child %d at node %d \n",child,nodeId);
 
 	if (childs.count(child) == 0){  // the target is unknown 
 		childs[child] = 1;
@@ -77,7 +77,25 @@ void nodeProcessor::init()
 
 void nodeProcessor::run()
 {
+	powerEvent t;
+	print_debug(3,"processcessor run: %d\n",nodeId);
 	while(1)
 	{
+		//print_debug(3,"node %d read queue %d\n",nodeId,inLink->pool_id);
+		t = inLink->pop();
+		print_debug(3,"node %d:received data sm %d ts %d power %f\n",nodeId,t.id,t.ts,t.power);
+/*
+		if (targets.count(t.id) != 0){  // the target is unknown 
+			if (targetLinks.count(targets[t.id]) != 0){ // the link is already set
+				targetLinks[targets[t.id]].push(t);
+			}
+			else{
+				print_error("SM does not exist %d\n",t.id);			
+			}
+		}
+		else{
+			print_error("SM does not exist %d\n",t.id);			
+		}
+*/
 	}
 }
